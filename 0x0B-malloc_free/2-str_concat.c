6678da42499041c672047d4d0e9d9e721943a9ea
Sara IBN-ELHAJ <sara.ibnelhaj@gmail.com>
2023-04-12 16:39:49 -0700

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,9 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 	for (len1 = 0; s1[len1]; len1++);
+
 	for (len2 = 0; s2[len2]; len2++);
+
 	concat_str = malloc((len1 + len2 + 1) *  sizeof(char));
 	if (concat_str == NULL)
 		return (NULL);
