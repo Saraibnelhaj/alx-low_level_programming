fc4f9bf7e5f676f08b413e7dbae9ff8935d78fd6
Sara IBN-ELHAJ <sara.ibnelhaj@gmail.com>
2023-04-12 15:34:58 -0700

    create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,7 +7,7 @@
  *@c: character to assign
  *return: a pointer to the array, otherwise NULL (if it fails)
  */
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 	char *arr;
 	unsigned int i;
