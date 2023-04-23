eff9488c695ac271dfa6de13d084a431e1072e61
Sara IBN-ELHAJ <sara.ibnelhaj@gmail.com>
2023-04-23 09:11:15 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,25 +14,29 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
-	unsigned int len1 = 0, len2 = 0;
+	unsigned int len1, len2, len;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 
 	if (n >= len2)
 		n = len2;
-	concat = malloc(sizeof(char) * (len1 + n + 1));
+	len = len1 + n;
+
+	concat = malloc(sizeof(char) * (len + 1));
 
 	if (concat == NULL)
 		return (NULL);
-	strncpy(concat, s1, len1);
-	strncpy(concat + len1, s2, n);
 
-	concat[len + n] = '\0';
+	memcpy(concat, s1, len1);
+	memcpy(concat + len1, s2, n);
+
+	concat[len] = '\0';
 
 	return (concat);
 }
