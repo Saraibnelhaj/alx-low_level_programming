70b03c225cef0283df3a5cfe704925961bf1c7da
Sara IBN-ELHAJ <sara.ibnelhaj@gmail.com>
2023-04-30 10:44:33 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
-void print_befrore_main(void) __attribute__ ((constructor));
+void print_before_main(void) __attribute__ ((constructor));
 /**
  *print_before_main - function that prints a message before
  *the main function is ex
@@ -11,15 +11,4 @@ void print_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-}
-/**
- *main - main function
- *
- *Return: always 0.
- */
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature,");
-	printf("challenges one to a race.)\n");
-	return (0);
 }
